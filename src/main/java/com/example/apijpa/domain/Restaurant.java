c81c022a9bd4cc115615a5ade553e57850ee078a
javierTallarin <javier1bravo@gmail.com>
2023-01-18 17:13:25 -0300

    added lombok to Restaurant.java

--- src/main/java/com/example/apijpa/domain/Restaurant.java
+++ src/main/java/com/example/apijpa/domain/Restaurant.java
@@ -1,6 +1,13 @@
 package com.example.apijpa.domain;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Restaurant {
+    String id;
     String name;
     String address;
     String numberPhone;
